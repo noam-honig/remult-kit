d9fd6b0312ebe20abe909203a53af3d03946fdcd
Noam Honig <noam.honig@gmail.com>
2023-12-31 12:32:51 +0200

    Added knex postgres

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,10 +1,13 @@
 import { remultExpress } from "remult/remult-express"
 import { createPostgresDataProvider } from "remult/postgres"
 import { GetDefinition } from "../shared/get-definition"
+import { createKnexDataProvider } from "remult/remult-knex"
 
 export const api = remultExpress({
-  dataProvider: createPostgresDataProvider({
-    connectionString: "postgres://postgres:MASTERKEY@localhost/postgres",
+  dataProvider: createKnexDataProvider({
+    // Knex client configuration for Postgres
+    client: "pg",
+    connection: "postgres://postgres:MASTERKEY@localhost/postgres",
   }),
   controllers: [GetDefinition],
 })
--- src/shared/get-definition.ts
+++ src/shared/get-definition.ts
@@ -1,8 +1,22 @@
-import { BackendMethod, SqlDatabase } from "remult"
+import { BackendMethod, SqlDatabase, remult } from "remult"
+import { KnexDataProvider } from "remult/remult-knex"
 
 export class GetDefinition {
   @BackendMethod({ allowed: true })
   static async getTableInfo(schema: string, name: string) {
+    return getStrategy().getTableInfo(schema, name)
+  }
+  @BackendMethod({ allowed: true })
+  static async getTables(): Promise<TableInfo[]> {
+    return getStrategy().getTables()
+  }
+}
+function getStrategy() {
+  if (remult.dataProvider instanceof SqlDatabase) return new PostgresSql()
+  else return new KnexPostgres()
+}
+class PostgresSql {
+  async getTableInfo(schema: string, name: string) {
     let c = await SqlDatabase.getDb().createCommand()
     let r = await c.execute(
       `select * from information_schema.columns where table_schema=${c.addParameterAndReturnSqlToken(
@@ -20,8 +34,7 @@ export class GetDefinition {
       }
     })
   }
-  @BackendMethod({ allowed: true })
-  static async getTables(): Promise<TableInfo[]> {
+  async getTables(): Promise<TableInfo[]> {
     return SqlDatabase.getDb()
       .execute(
         "select * from information_schema.tables where table_schema not in ('pg_catalog','information_schema')"
@@ -30,6 +43,55 @@ export class GetDefinition {
         r.rows.map((x) => {
           //    return x
           return { schema: x.table_schema, name: x.table_name }
+        })
+      )
+  }
+}
+class KnexPostgres implements PostgresSql {
+  getTableInfo(
+    schema: string,
+    name: string
+  ): Promise<
+    {
+      name: any
+      type: any
+      nullable: boolean
+      maxLength: any
+      datetime_precision: any
+      column_default: any
+    }[]
+  > {
+    const db = KnexDataProvider.getDb()
+    return db
+      .select("*")
+      .from("information_schema.columns")
+      .where("table_schema", schema)
+      .where("table_name", name)
+      .then((r) =>
+        r.map((x) => {
+          return {
+            name: x.column_name,
+            type: x.data_type,
+            nullable: x.is_nullable == "YES",
+            maxLength: x.character_maximum_length,
+            datetime_precision: x.datetime_precision,
+            column_default: x.column_default,
+          }
+        })
+      )
+  }
+  getTables(): Promise<TableInfo[]> {
+    const db = KnexDataProvider.getDb()
+    return db
+      .select("*")
+      .from("information_schema.tables")
+      .whereNotIn("table_schema", ["pg_catalog", "information_schema"])
+      .then((r) =>
+        r.map((x) => {
+          return {
+            schema: x.table_schema,
+            name: x.table_name,
+          }
         })
       )
   }
