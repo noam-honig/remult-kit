88254685ad2ebd646ec5dd64db8c1d42f66090cb
jycouet <jycouet@gmail.com>
2024-01-18 15:37:08 +0100

    move files

--- src/lib/ROUTES.ts
+++ src/lib/ROUTES.ts
@@ -13,6 +13,7 @@ const PAGES = {
   '/raw-db': `/raw-db`,
   '/schemas': `/schemas`,
   '/settings': `/settings`,
+  '/admin': `/admin`,
 }
 
 /**
@@ -136,7 +137,7 @@ export function route<T extends keyof AllTypes>(key: T, ...params: any[]): strin
  * ```
  */
 export type KIT_ROUTES = {
-  PAGES: { '/': never; '/raw-db': never; '/schemas': never; '/settings': never }
+  PAGES: { '/': never; '/raw-db': never; '/schemas': never; '/settings': never; '/admin': never }
   SERVERS: Record<string, never>
   ACTIONS: Record<string, never>
   LINKS: Record<string, never>
