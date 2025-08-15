8315495d1851f2a1289d6d03e308937c32f6ad55
jycouet <jycouet@gmail.com>
2025-08-15 23:15:40 +0200

    :construction: WIP: remult kit & sveltekit

---
+++ src/routes/api/[...remult]/+server.ts
@@ -0,0 +1,3 @@
+import { api } from '../../../server/api'
+
+export const { GET, POST, PUT, DELETE } = api
--- src/server/api.ts
+++ src/server/api.ts
@@ -1,26 +1,11 @@
 import { JsonDataProvider, repo } from 'remult'
-import { remultSveltekit } from 'remult/remult-sveltekit'
+import { remultApi } from 'remult/remult-sveltekit'
 import { JsonEntityFileStorage } from 'remult/server'
 
 import { ActionsController } from '$shared/controllers/ActionsController'
 import { Setting, SettingKey } from '$shared/entities/Setting'
 
-// One day maybe!
-// const dynamicEntities = []
-// try {
-//   const files = getFilesUnder('./src/shared/entities')
-
-//   for (let i = 0; i < files.length; i++) {
-//     const className = files[i].replaceAll('.ts', '')
-//     /* @vite-ignore */
-//     const imp = await import('../shared/entities/' + className)
-//     dynamicEntities.push(imp[className])
-//   }
-// } catch (error) {}
-
-// const u = await import('../shared/entities/User')
-
-export const handleRemult = remultSveltekit({
+export const api = remultApi({
   dataProvider: async () => new JsonDataProvider(new JsonEntityFileStorage('.remult-kit')),
 
   logApiEndPoints: false,
