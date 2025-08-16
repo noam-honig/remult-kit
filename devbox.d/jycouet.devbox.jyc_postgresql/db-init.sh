d70c4334fb955f8ff2fccd620dc7b8e9a0046af5
jycouet <jycouet@gmail.com>
2025-08-16 09:56:27 +0200

    format is always a pain! I need to update my kitql S***

--- devbox.d/jycouet.devbox.jyc_postgresql/db-init.sh
+++ devbox.d/jycouet.devbox.jyc_postgresql/db-init.sh
@@ -13,4 +13,4 @@ source "$jyc_postgresql_dir/helpers.sh"
 
 # additionals examples
 execute_and_log "Database timezone to UTC enforcing" "psql -d \"$DB_NAME\" -c \"ALTER DATABASE \\\"$DB_NAME\\\" SET timezone TO 'UTC';\""
-execute_and_log "Timezone check" 'echo "Timezone: $(psql -U postgres -d "'$DB_NAME'" -t -A -c "SHOW timezone;")"' 
+execute_and_log "Timezone check" 'echo "Timezone: $(psql -U postgres -d "'$DB_NAME'" -t -A -c "SHOW timezone;")"'
